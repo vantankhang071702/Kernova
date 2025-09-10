7c669f79a67203745ce3369aceb8112c18664bc6
Khang.Van <vantankhang0717@gmail.com>
2025-09-10 20:12:13 +0700

    Using macro to create GDT entry

--- kernel/arch/i386/gdt.c
+++ kernel/arch/i386/gdt.c
@@ -1,15 +1,21 @@
-#include <gdt.c>
+#include <gdt.h>
 
-void set_gdt_entry(struct gdt *entry,
-		   uint32_t base,
-		   uint32_t limit,
-		   uint8_t acess;
-		   uint8_t gran) 
+uint64_t create_descriptor(uint32_t base, uint32_t limit, uint16_t flag)
 {
-	entry->limit_low = limit & 0xFF;
-	entry->base_low = base & 0xFF;
-	entry->base_mid = (base >> 16) & 0x0F;
-	entry->access = access;
-	entry->granularity = (limit >> 16) & 0x0F | (gran & 0xF0);
-	entry->base_high = (base >> 24) && 0xFF;
+    uint64_t descriptor;
+
+    // Create the high 32 bit segment
+    descriptor = limit & 0x000F0000; // set limit bits 19:16
+    descriptor |= (flag << 8) & 0x00F0FF00; // set type, p, dpl, s, g, d/b, l and avl fields 
+    descriptor |= (base >> 16) & 0x000000FF; // set base bits 23:16 
+    descriptor |= base & 0xFF000000; // set base bits 31:24
+
+    // Shift by 32 to allow for low part of segment
+    descriptor <<= 32;
+
+    // Create the low 32 bit segment
+    descriptor |= base << 16; // set base bits 15:0 
+    descriptor |= limit & 0x0000FFFF; // set limit bits 15:0
+    
+    return descriptor;
 }
